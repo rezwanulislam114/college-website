46f0001c08f2c3b89040eec82a08555bfa3e37bb
rezwanulisalam114 <rezwanulislam114@gmail.com>
2021-07-04 23:15:11 +0600

    completed home page

---
+++ script.js
@@ -0,0 +1,11 @@
+let nev = document.getElementById("navlink")
+let close = document.getElementById("close")
+
+function showMenu(){
+    navlink.style.width = "250px";
+    close.style.display = "block"
+}
+function hideMenu(){
+    navlink.style.width = "0";
+    close.style.display = "none"
+}
